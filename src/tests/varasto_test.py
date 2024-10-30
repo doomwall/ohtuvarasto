20441a5498d1e58270b985a4ea54997eed06362c
doomwall <tuomo.valkila@gmail.com>
2024-10-30 19:48:42 +0200

    coverage setup, test file updated

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -28,8 +28,13 @@ class TestVarasto(unittest.TestCase):
         self.varasto.lisaa_varastoon(8)
 
         saatu_maara = self.varasto.ota_varastosta(2)
-
         self.assertAlmostEqual(saatu_maara, 2)
+
+        saatu_maara2 = self.varasto.ota_varastosta(-1)
+        self.assertAlmostEqual(saatu_maara2, 0)
+
+        saatu_maara3 = self.varasto.ota_varastosta(50)
+        self.assertAlmostEqual(saatu_maara3, 6)
 
     def test_ottaminen_lisaa_tilaa(self):
         self.varasto.lisaa_varastoon(8)
@@ -38,3 +43,30 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_lisataan_likka(self):
+        self.varasto.lisaa_varastoon(1000)
+
+        self.assertAlmostEqual(self.varasto.saldo, self.varasto.tilavuus)
+
+    def test_lisataan_negatiivinen(self):
+        saldo_ennen = self.varasto.saldo
+        self.varasto.lisaa_varastoon(-10)
+
+        self.assertAlmostEqual(self.varasto.saldo, saldo_ennen)
+
+    def test_negatiivinen_varasto_luonti(self):
+        uusi_varasto = Varasto(-10, -10)
+        self.assertAlmostEqual(uusi_varasto.saldo, 0)
+        self.assertAlmostEqual(uusi_varasto.saldo, 0)
+
+    def test_alkusaldo_enemmankuin_tilavuus(self):
+        uusi_varasto = Varasto(10, 15)
+        
+        self.assertAlmostEqual(uusi_varasto.saldo, 10)
+
+
+    def test_teksti(self):
+        tulostus = str(self.varasto)
+
+        self.assertEqual(tulostus, "saldo = 0, vielä tilaa 10")
